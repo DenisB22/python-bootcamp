67d0a84b59d6e20a36da8e48c2e832cc23f7647e
DenisB22 <bechiragich97@gmail.com>
2023-07-21 17:03:43 +0300

    Added Module communicator

--- web_scrape/controller.py
+++ web_scrape/controller.py
@@ -4,18 +4,8 @@ from web_scrape.export_min_to_csv import export_only_rows_to_csv_min, export_row
 from web_scrape.web_scraper import web_scrape
 
 
-# Controller which handles the creation of the first and second website
-def controller() -> None:
-    # First Website
-    first_website = web_scrape("https://bianchi.bg/kafe-na-zarna", ".product-thumb.product-wrapper .product-details",
-                               ".product-thumb.product-wrapper .product-details .price",
-                               ".product-thumb.product-wrapper .product-details .name",
-                               ".product-thumb.product-wrapper .image .first-image", "data-src")
-
-    # Extract the product with maximum and minimum price which will be sent to the function handling the export to CSV
-    product_max = first_website[0]
-    product_min = first_website[1]
-
+# Communicator with CSV exporter module
+def module_communicator(product_max, product_min):
     file_exists = export_only_cols_to_csv_max()
 
     if file_exists:
@@ -26,6 +16,22 @@ def controller() -> None:
         # Call the functions which handle adding both the column headers and the information of the first product
         export_rows_and_cols_to_csv_max(product_max)
         export_rows_and_cols_to_csv_min(product_min)
+
+
+# Controller which handles the creation of the first and second website
+def controller() -> None:
+
+    # First Website
+    first_website = web_scrape("https://bianchi.bg/kafe-na-zarna", ".product-thumb.product-wrapper .product-details",
+                               ".product-thumb.product-wrapper .product-details .price",
+                               ".product-thumb.product-wrapper .product-details .name",
+                               ".product-thumb.product-wrapper .image .first-image", "data-src")
+
+    # Extract the product with maximum and minimum price which will be sent to the function handling the export to CSV
+    product_max = first_website[0]
+    product_min = first_website[1]
+
+    module_communicator(product_max, product_min)
 
     # Second Website
     second_website = web_scrape(
@@ -38,16 +44,7 @@ def controller() -> None:
     product_max = second_website[0]
     product_min = second_website[1]
 
-    file_exists = export_only_cols_to_csv_max()
-
-    if file_exists:
-        # Call the functions which handle the adding of only rows, because we already have column headers
-        export_only_rows_to_csv_max(product_max)
-        export_only_rows_to_csv_min(product_min)
-    else:
-        # Call the functions which handle adding both the column headers and the information of the first product
-        export_rows_and_cols_to_csv_max(product_max)
-        export_rows_and_cols_to_csv_min(product_min)
+    module_communicator(product_max, product_min)
 
 
 if __name__ == '__main__':
